0e62b9cd96a7631d13b42340af6b5ed631f18a9a
NikitOS-ssd <exemplum777@gmail.com>
2023-03-01 10:38:41 +0600

    Create delete and update methods for todo lists

--- todo.go
+++ todo.go
@@ -1,5 +1,7 @@
 package todo
 
+import "errors"
+
 type TodoList struct {
 	ID          int    `json:"-" db:"id"`
 	Title       string `json:"title" db:"title" binding:"required"`
@@ -23,4 +25,17 @@ type ListItem struct {
 	ID     int
 	ListID int
 	ItemID int
+}
+
+type UpdateListInput struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+}
+
+func (i UpdateListInput) Validate() error {
+	if i.Title == nil && i.Description == nil {
+		return errors.New("update structure has no values")
+	}
+
+	return nil
 }
